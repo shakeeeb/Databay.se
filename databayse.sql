2f28017b5c81f9de3e3c9a90eb1aca1468558980
myip <miuki.yip@cs.stonybrook.edu>
2015-10-19 20:01:59 -0400

    Moved Shakeeb's stuff into the corret file for the addAuction elements

--- databayse.sql
+++ databayse.sql
@@ -121,15 +121,25 @@ insert into DATABAYSE.Employee(SSN, FirstName, LastName, Address, City,
 End
 $$
 
-CREATE PROCEDURE addItem(IN itemID INTEGER, IN itemName CHAR(20),
-IN itemType CHAR(12), IN itemYear INTEGER, IN itemCopiesSold INTEGER, IN itemAmountInStock
-INTEGER)
 
 CREATE PROCEDURE addItem(IN itemName CHAR(20), IN itemType CHAR(12), IN itemYear INTEGER, IN itemAmountInStock INTEGER)
 BEGIN
 insert into DATABAYSE.Item(Name, Type, Year, AmountInStock)
   values(itemName, itemType, itemYear, itemAmountInStock);
 
+End
+$$
+
+CREATE PROCEDURE addAuction(IN seller_id CHAR(32), IN item_id INTEGER, IN employee_id INTEGER, IN opening_bid DECIMAL(8,2) IN reserve DECIMAL(8,2))
+BEGIN
+start_date DATE = CURRENT_DATE;
+start_time TIME = CURRENT_TIME;
+closing_date DATE = ADDDATE(start_date, INTERVAL 3 DAY);
+closing_time TIME = start_time; #defaults exactly 3 days later
+increment DECIMAL(8,2) = opening_bid/8;# let the default increment be 1/8 the initial price
+insert into DATABAYSE.Auction(SellerID, ItemID, EmployeeID, OpeningBid, OpeningDate, OpeningTime, ClosingDate, ClosingTime, Reserve, Increment)
+  values(Lower(seller_id), item_id, employee_id, opening_bid, start_date, start_time , closing_date, closing_time, reserve, increment
+    );
 End
 $$
 
