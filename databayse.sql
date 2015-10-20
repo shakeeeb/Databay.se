e91a850e0ba17c05046ecc7b71077a636c18fc66
myip <miuki.yip@cs.stonybrook.edu>
2015-10-19 20:27:13 -0400

    Making sure all changes are commited

--- databayse.sql
+++ databayse.sql
@@ -105,12 +105,7 @@ End
 $$
 
 
-CREATE PROCEDURE testSelect()
-BEGIN
-SELECT *
-FROM Customer;
-
-CREATE PROCEDURE addEmployee(IN empl_ssn, IN empl_fn CHAR(32), IN empl_ln CHAR(32),
+CREATE PROCEDURE addEmployee(IN empl_ssn CHAR(14), IN empl_fn CHAR(32), IN empl_ln CHAR(32),
 IN empl_addr CHAR(128), IN empl_city CHAR(32), IN empl_state CHAR(2), IN empl_zip
 INTEGER, IN empl_tel CHAR(20), IN empl_sd DATE, IN empl_hr INTEGER)
 BEGIN
@@ -130,13 +125,14 @@ insert into DATABAYSE.Item(Name, Type, Year, AmountInStock)
 End
 $$
 
-CREATE PROCEDURE addAuction(IN seller_id CHAR(32), IN item_id INTEGER, IN employee_id INTEGER, IN opening_bid DECIMAL(8,2) IN reserve DECIMAL(8,2))
+
+CREATE PROCEDURE addAuction(IN seller_id CHAR(32), IN item_id INTEGER, IN employee_id INTEGER, IN opening_bid DECIMAL(8,2), IN reserve DECIMAL(8,2))
 BEGIN
 start_date DATE = CURRENT_DATE;
 start_time TIME = CURRENT_TIME;
 closing_date DATE = ADDDATE(start_date, INTERVAL 3 DAY);
 closing_time TIME = start_time; #defaults exactly 3 days later
-increment DECIMAL(8,2) = opening_bid/8;# let the default increment be 1/8 the initial price
+increment DECIMAL(8,2) = opening_bid/8; #let the default increment be 1/8 the initial price
 insert into DATABAYSE.Auction(SellerID, ItemID, EmployeeID, OpeningBid, OpeningDate, OpeningTime, ClosingDate, ClosingTime, Reserve, Increment)
   values(Lower(seller_id), item_id, employee_id, opening_bid, start_date, start_time , closing_date, closing_time, reserve, increment
     );
