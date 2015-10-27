a8d6cd9de07e81f047061c2b64c14dfc0e44193e
myip <miuki.yip@cs.stonybrook.edu>
2015-10-26 20:19:14 -0400

    Updated Data

--- demodata/employee_data.sql
+++ demodata/employee_data.sql
@@ -5,3 +5,18 @@ call addEmployee('789-12-3456', 'David', 'Warren', '456 Sunken Street',
   'Stony Brook', 'NY', 11794, '(516)632-9987', DATE('1999-2-2'),50);
 
 call promoteToManager('789-12-3456');
+
+call addEmployee('123-22-4321', 'Steve', 'Skiena', 'Algoville',
+  'Prim', 'KR', 43210, '(456)789-432', DATE('1990-11-4'), 373);
+
+call addEmployee('321-45-3432', 'Yanni', 'Liu', 'Network Drive',
+  'Router', 'GR', 77788, '(787)778-2345', DATE('3004-12-4'), 310);
+
+call addEmployee('239-22-0000', 'Jason', 'Fontaine', 'Google Town',
+  'Happy', 'BK', 39021, '(482)304-2842', DATE('2015-1-3'), 220);
+
+call addEmployee('345-92-2039', 'Mario', 'Plumber', 'Pipeline Drive',
+  'Mushroom City', 'MW', 34509, '(098)324-7654', DATE('2012-1-4'), 190);
+
+call addEmployee('302-03-9210', 'Homer', 'SImpson', 'Simpson Street',
+  'Springfield', 'IL', 00996, '(543)123-7683', DATE('1927-1-4'), 178);
--- demodata/item_data.sql
+++ demodata/item_data.sql
@@ -9,10 +9,11 @@ call addItem('Titanic', 'DVD', 2005, 4);
 call addItem('Nissan Sentra', 'Car', 2007, 1);
 call addItem('Titanic', 'DVD', 2005, 5);
 
-call addItem('Audi R8', 'Car', 2010, 1);
-call addItem('Nissan Sentra', 'Car', 2009, 1);
+call addItem('delicious candy', 'food', 2006, 200);
+call addItem('rainbow unicorn', 'Toy', 2012, 10);
+call addItem('Butterfly Buttercup', 'Toy', 2004, 3);
+call addItem('mixtapes', 'music', 2009, 50);
+call addItem('batmobile', 'car', 1983, 1);
+call addItem('Pikachu', 'other', 1994, 2);
+call addItem('garbage', 'other', 1980, 55);
 
-
-update Item SET CopiesSold = 1 where ItemID = 1;
-update Item SET CopiesSold = 2 where ItemID = 2;
-update Item SET CopiesSold = 3 where ItemID = 3;
