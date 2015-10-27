5c81c903a3b165c52c2a4076a0ac7bd5eb33506b
Terrell <terrell.mack@stonybrook.edu>
2015-10-26 20:33:52 -0400

    yo

--- demodata/bid_data.sql
+++ demodata/bid_data.sql
@@ -3,7 +3,6 @@ call addBid(1, 'shiyong', 9, 10);
 call addBid(1, 'shiyong', 10, 10);
 call addBid(1, 'shiyong', 11, 15);
 
-
 call addBid(2, 'spongebob', 6, 10);
 call addBid(2, 'tom', 8, 10);
 call addBid(2, 'spongebob', 10, 12);
