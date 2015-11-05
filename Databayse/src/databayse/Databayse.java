684e57e1d919819a84cdb448aa32020907085238
Terrell <terrell.mack@stonybrook.edu>
2015-11-04 20:03:44 -0500

    Adding JDBC

---
+++ Databayse/src/databayse/Databayse.java
@@ -0,0 +1,69 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Terrell Mack
+ */
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dbconnection;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+/**
+ *
+ * @author Terrell Mack
+ */
+//STEP 1. Import required packages
+
+public class Databayse {
+
+    public static void main(String[] args) {
+            
+        
+        Connection conn = null;
+        Statement stmt = null;
+        try {
+            // The newInstance() call is a work around for some
+            // broken Java implementations
+        Class.forName("com.mysql.jdbc.Driver");//.newInstance();
+        conn = DriverManager.getConnection("jdbc:mysql://localhost/DATABAYSE","root","1");
+        stmt = conn.createStatement();
+       
+        String query = "Select * from Employee";
+        ResultSet res = stmt.executeQuery(query);
+        
+        while(res.next()){
+         //Retrieve by column name
+         int id  = res.getInt("EmployeeID");
+         String firstName = res.getString("FirstName");
+         String lastName = res.getString("LastName");
+
+         //Display valuesa
+         System.out.print("ID: " + id);
+         System.out.print(" FirstName: " + firstName);
+         System.out.print(" LastName: " + lastName);
+         System.out.println();
+      
+      }
+           
+        } catch (Exception e) {
+            
+            System.out.println("It didn't work :D" + e);
+        }
+    }
+    
+    
+    }
+
+
