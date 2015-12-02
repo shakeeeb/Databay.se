7c5bb1e958692edf243cfe493ae7231673cb219e
Shakeeb <shakeeb8644@bitbucket.org>
2015-12-02 18:38:52 -0500

    adding demodata

--- demodata/item_data.sql
+++ demodata/item_data.sql
@@ -3,6 +3,7 @@ Adds Customer Demo Data to DATABAYSE
 Example: (you can copy and paste this to add new data)
 call addCustomer('firstname','lastname', 'address', 'city', 'state',
 zip, 'telephone', 'email','creditcard');
+call addItem(name, type, year, );
 *******************************************************************************/
 
 call addItem('Titanic', 'DVD', 2005, 3);
@@ -16,3 +17,35 @@ call addItem('Watch', 'Accessory', 2009, 50);
 call addItem('batmobile', 'car', 1983, 1);
 call addItem('Pikachu', 'other', 1994, 2);
 call addItem('garbage', 'other', 1980, 55);
+
+call addItem('Toyota Camry', 'car', 2012, 1);
+call addItem('Honda Civic', 'car', 2010, 1);
+call addItem('Mazda Miata', 'car', 2008, 2);
+
+call addItem('Santa Hat','clothes',2015,20);
+call addItem('elf outfit','clothes',2012,2);
+call addItem('eyepatches', 'clothes',2009,30);
+call addItem('ruffled shirt','clothes', 1882, 1);
+call addItem('Tricorne','clothes', 1776, 1);
+call addItem('Straw Hat', 'clothes', 2010, 1);
+
+call addItem('Game Boy Advance','Toy', 2005, 2);
+call addItem('Goku action figure', 'Toy', 1995, 1);
+call addItem('bobble-head', 'Toy', 2000, 4);
+call addItem('One Piece Game', 'Toy', 2010, 1);
+
+call addItem('IPad', 'electronics', 2012, 4);
+call addItem('Samsung Galaxy S4', 'electronics', 2014, 12);
+call addItem('Roomba', 'electronics', 2007, 7);
+
+call addItem('Limes', 'grocery', 2014, 3000);
+call addItem('Rum', 'grocery', 1822, 50);
+call addItem('Pomegranate', 'grocery', 2001, 8);
+call addItem('Pumpkin Spice Latte', 'grocery', 2012, 1);
+call addItem('Cheese', 'grocery', 2003, 50);
+call addItem('Remy Martin', 'grocery', 1738, 679);
+
+call addItem('Pirate Ship', 'other', 1764, 1);
+call addItem('Pirate Gold', 'other', 1752, 1000);
+call addItem('pegleg', 'other', 1783, 2);
+call addItem('Hippo', 'other', 2014, 1);
